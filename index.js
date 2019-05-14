e28cee94e2cedddb6b46d5076a70da279c638271
trivasco <andrea.vasco@outlook.com>
2019-05-14 17:09:43 +0200

    /* Stable version - local hosting */

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
-/*requiring express*/
+/*requiring express and other dependencies*/
 var express = require('express');
+var path = require('path');
+var https = require('https');
+var qlikauth = require('qlik-auth');
+
 
 /*instantiating an instance*/
 var app = express();
 
+/*defining middleware for static pages*/
+app.use(express.static(path.join(__dirname, 'public')));
 
 /*Routes
 respond index.hmtl when the user hits the homepage*/
+
+
 app.get('/', function(req, res)
 {
     res.send('this will be the homepage!');
 });
 
 
-/*Web server port to listen upon to*/
+  /*Web server port to listen upon to*/
 app.listen(3000);
-
-git 
